ed946b1cbbfe94e93c3b038d01fede620c2b3596
jason <jason.cheng@lileesystems.com>
2021-03-03 00:23:20 +0800

    add angular full page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'challenge30day';
+
+  config:unknown;
+  fullpage_api:unknown;
+
+  constructor() {
+
+    // this is just an example => for more details on config please visit fullPage.js docs
+    this.config = {
+      licenseKey: 'YOUR LICENSE KEY HERE',
+      sectionsColor: ['#7BAABE', 'whitesmoke', '#7BAABE', 'whitesmoke', '#7BAABE'],
+      anchors: ['p1', 'p2', 'p3', 'p4', 'p5'],
+      navigation: true,
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  getRef(fullPageRef:unknown) {
+    this.fullpage_api = fullPageRef;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +17,8 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'dark' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    AngularFullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
