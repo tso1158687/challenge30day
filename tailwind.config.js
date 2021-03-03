5fbbed236f0773ba4725330afb1c981f658945f5
jason <jason.cheng@lileesystems.com>
2021-03-03 13:04:02 +0800

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  prefix: "",
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  // plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+};
